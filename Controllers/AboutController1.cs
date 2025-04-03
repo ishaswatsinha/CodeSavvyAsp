61e8d066b0001608f6a327afd1262c0172d60edc
Divyanshu Raj <160339078+Divyanshuverma235@users.noreply.github.com>
2025-04-03 20:42:24 +0530

    About controller added

---
+++ Controllers/AboutController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSavvyAsp.Controllers
+{
+    public class AboutController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
