1437ee434f2deb1b1742651eab35508beb44ccb6
Shaswat <shaswatsinha87@gmail.com>
2025-04-04 22:08:17 +0530

    Add SAccountDetails

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using CodeSavvyAsp.Data;
-using CodeSavvyAsp.net.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
